ab06af4b82d8d7f122d751d97abbc38c864a3e3f
PhamQuangBao <67539361+PhamQuangBao@users.noreply.github.com>
2021-10-27 12:11:03 +0700

    Show detail products

--- src/components/section/Details.js
+++ src/components/section/Details.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { DataContext } from '../Context';
+import { Link } from 'react-router-dom';
+import '../css/Details.css'
 
 export class Details extends Component {
     static contextType = DataContext;
@@ -10,15 +12,41 @@ export class Details extends Component {
     getProduct = () =>{
         if(this.props.match.params.id){
             const res = this.context.products;
+            const data = res.filter(item => {
+                return item._id === this.props.match.params.id
+            })
+            //Goi den setState de cap nhat lai kho luu tru state vi khong the gian truc tiep vao state nhu cac bien thong thuong
+            this.setState({product: data})
         }
-        console.log(res)
+    };
+
+    componentDidMount(){
+        this.getProduct()
     }
 
     render() {
+        const {product} = this.state;
         return(
-            <div>
-                {this.props.match.params.id}
-            </div>
+            <>
+                {
+                    product.map(product =>(
+                        <div className="details" key={product._id}>
+                            <img src={process.env.PUBLIC_URL + product.src} alt=""/>
+                            <div className="box">
+                                <div className="row">
+                                    <h2>{product.title}</h2>
+                                    <span>${product.price}</span>
+                                </div>
+                                <p>{product.Description}</p>
+                                <p>{product.Content}</p>
+                                <Link to="/cart" className="cart">
+                                    Thêm vào giỏ
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                }
+            </>
         )
     }
 }
